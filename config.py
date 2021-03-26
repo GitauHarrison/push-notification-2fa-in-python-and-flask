e44eed5df7403e5a1e42a0201d1de6e002807206
harry <harrisongitau3@gmail.com>
2021-03-26 04:38:21 +0300

    Add environment variables

--- config.py
+++ config.py
@@ -17,3 +17,16 @@ class Config(object):
 
     # Form protection
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Authy configurations
+    AUTHY_APP_NAME = os.environ.get('AUTHY_APP_NAME')
+    AUTHY_APP_ID = os.environ.get('AUTHY_APP_ID')
+    AUTHY_PRODUCTION_API_KEY = os.environ.get('AUTHY_PRODUCTION_API_KEY')
+
+    # Email configurations
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
