1af01ccf3dc04ab0ef2446d70be443d54248eb8d
harry <harrisongitau3@gmail.com>
2021-03-27 05:24:22 +0300

    Fix error handling

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,14 +3,14 @@ from app.errors import bp
 from app import db
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html',
                            title='Page Not Found',
                            ), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.roll_back()
     return render_template('errors/500.html',
